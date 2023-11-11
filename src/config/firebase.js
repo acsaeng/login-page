bd6b8d30b1850c5c5992d31a2b419f63cc3983a2
Aron Saengchan <acsaeng@gmail.com>
2023-11-11 16:23:07 -0700

    Removed API key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// Add your Firebase SDK configuration parameters here
 const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
